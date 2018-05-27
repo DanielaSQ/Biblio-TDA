efeb28cd426011c321b2a918bde2dbf8e207d618
daniela <daniela@DESKTOP-8N3GFQ6>
2018-05-26 21:22:06 -0400

    Creacion menu Principal;creacion de Formularios de Cliente,Trabajador y Proveedor

--- src/biblio/Main.java
+++ src/biblio/Main.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package biblio;
+import javax.swing.*;
 
 /**
  *
@@ -15,7 +12,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.setVisible(true);
+        
+        menuPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        
+        
+    }   
 }
+
